a3374da9a5941fbc135826a46f4824392233654d
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-24 12:28:08 +0300

    Initialize Spring Boot skeleton

---
+++ src/main/java/com/jb/cloudstorage/cloud_storage/CloudFileStorageApplication.java
@@ -0,0 +1,11 @@
+package com.jb.cloudstorage.cloud_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileStorageApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFileStorageApplication.class, args);
+	}
+}
