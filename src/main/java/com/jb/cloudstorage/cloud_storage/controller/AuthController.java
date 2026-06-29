54a53f3b6a3777832a49ad4db79e354e7ff85928
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-29 13:42:06 +0300

    Add @Transactional to registration

---
+++ src/main/java/com/jb/cloudstorage/cloud_storage/controller/AuthController.java
@@ -0,0 +1,2 @@
+package com.jb.cloudstorage.cloud_storage.controller;public class AuthController {
+}
--- src/main/java/com/jb/cloudstorage/cloud_storage/service/AuthService.java
+++ src/main/java/com/jb/cloudstorage/cloud_storage/service/AuthService.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AuthService {
@@ -24,6 +25,7 @@ public class AuthService {
         this.authenticationManager = authenticationManager;
     }
 
+    @Transactional
     public UserResponse register(SignUpRequest signUpRequest) {
         String username = signUpRequest.username();
         UserEntity userEntity = userRepository.findByUsername(username);
