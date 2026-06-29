92f4ce6f40521ec93af84d71a490994d41babba7
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-29 13:59:04 +0300

    Add user registration in AuthController

--- src/main/java/com/jb/cloudstorage/cloud_storage/controller/AuthController.java
+++ src/main/java/com/jb/cloudstorage/cloud_storage/controller/AuthController.java
@@ -1,2 +1,25 @@
-package com.jb.cloudstorage.cloud_storage.controller;public class AuthController {
+package com.jb.cloudstorage.cloud_storage.controller;
+
+import com.jb.cloudstorage.cloud_storage.dto.SignUpRequest;
+import com.jb.cloudstorage.cloud_storage.dto.UserResponse;
+import com.jb.cloudstorage.cloud_storage.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/sign-up")
+    public UserResponse register(@Valid @RequestBody SignUpRequest request) {
+        return authService.register(request);
+    }
 }
