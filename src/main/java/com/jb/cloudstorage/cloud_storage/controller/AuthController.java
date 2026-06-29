bd526b1f71709f67e9a52adde5cb969be6257c77
J-Bakuli <julia.bakuli@yandex.ru>
2026-06-29 14:38:04 +0300

    Return 201 Created for sign-up

--- src/main/java/com/jb/cloudstorage/cloud_storage/controller/AuthController.java
+++ src/main/java/com/jb/cloudstorage/cloud_storage/controller/AuthController.java
@@ -4,9 +4,11 @@ import com.jb.cloudstorage.cloud_storage.dto.SignUpRequest;
 import com.jb.cloudstorage.cloud_storage.dto.UserResponse;
 import com.jb.cloudstorage.cloud_storage.service.AuthService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +20,7 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/sign-up")
     public UserResponse register(@Valid @RequestBody SignUpRequest request) {
         return authService.register(request);
