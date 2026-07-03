45a170eff6212e5f891e90e6d2f1f3dd2cc49468
J-Bakuli <julia.bakuli@yandex.ru>
2026-07-03 13:10:22 +0300

    Add ensureBucketExists and uploadFile in FileStorageService

---
+++ src/main/java/com/jb/cloudstorage/cloud_storage/service/FileStorageService.java
@@ -0,0 +1,72 @@
+package com.jb.cloudstorage.cloud_storage.service;
+
+import com.jb.cloudstorage.cloud_storage.config.MinioProperties;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.errors.MinioException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+public class FileStorageService {
+    private final MinioProperties minioProperties;
+    private final MinioClient minioClient;
+
+    public FileStorageService(MinioProperties minioProperties, MinioClient minioClient) {
+        this.minioProperties = minioProperties;
+        this.minioClient = minioClient;
+    }
+
+    public void ensureBucketExists() throws MinioException, IOException, NoSuchAlgorithmException, InvalidKeyException {
+        boolean exists = minioClient.bucketExists(
+                BucketExistsArgs.builder()
+                        .bucket(minioProperties.bucket())
+                        .build()
+        );
+
+        if (!exists) {
+            minioClient.makeBucket(
+                    MakeBucketArgs.builder()
+                            .bucket(minioProperties.bucket())
+                            .build()
+            );
+        }
+    }
+
+    public String uploadFile(Long userId, String relativePath, MultipartFile file) throws MinioException, IOException, NoSuchAlgorithmException, InvalidKeyException {
+        ensureBucketExists();
+
+        String objectPath = joinPath(relativePath, file.getOriginalFilename());
+        String objectName = fullObjectName(userId, objectPath);
+
+        minioClient.putObject(PutObjectArgs.builder()
+                .bucket(minioProperties.bucket())
+                .object(objectName)
+                .stream(file.getInputStream(), file.getSize(), -1)
+                .contentType(file.getContentType())
+                .build());
+        return objectName;
+    }
+
+    private String userRootPrefix(Long userId) {
+        return "user-" + userId + "-files/";
+    }
+
+    private String fullObjectName(Long userId, String objectRelativePath) {
+        return userRootPrefix(userId) + objectRelativePath;
+    }
+
+    private String joinPath(String folderPath, String fileName) {
+        if (folderPath == null || folderPath.isBlank()) {
+            return fileName;
+        }
+        String normalized = folderPath.endsWith("/") ? folderPath : folderPath + "/";
+        return normalized + fileName;
+    }
+}
