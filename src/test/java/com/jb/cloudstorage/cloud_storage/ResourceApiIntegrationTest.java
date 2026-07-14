ab9dad5ddb1cc8a1f4d678ef8496d7a7c93bf1b5
J-Bakuli <julia.bakuli@yandex.ru>
2026-07-14 14:54:34 +0300

    Add disabled move/rename file test cases

--- src/test/java/com/jb/cloudstorage/cloud_storage/ResourceApiIntegrationTest.java
+++ src/test/java/com/jb/cloudstorage/cloud_storage/ResourceApiIntegrationTest.java
@@ -6,11 +6,11 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,7 +45,8 @@ public class ResourceApiIntegrationTest extends BaseApiIntegrationTest {
                         get("/api/resource")
                                 .param("path", "noFile.txt")
                                 .session(session))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", containsString("not found")));
     }
 
     @Test
@@ -120,7 +121,8 @@ public class ResourceApiIntegrationTest extends BaseApiIntegrationTest {
                         delete("/api/resource")
                                 .param("path", "noFile.txt")
                                 .session(session))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", containsString("is not found")));
     }
 
     @Test
@@ -195,7 +197,8 @@ public class ResourceApiIntegrationTest extends BaseApiIntegrationTest {
                         get("/api/resource/download")
                                 .param("path", "noFile.txt")
                                 .session(session))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", containsString("is not found")));
     }
 
     @Test
@@ -224,5 +227,152 @@ public class ResourceApiIntegrationTest extends BaseApiIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(header().string("Content-Type", containsString("octet-stream")))
                 .andExpect(content().bytes(content));
+    }
+
+    @Test
+    void testMoveFile_unauthorized() throws Exception {
+        mockMvc.perform(
+                        post("/api/resource/move")
+                                .param("from", "exam/test.txt")
+                                .param("to", "test/test.txt"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @Disabled
+    void testMoveFile_notFound() throws Exception {
+        basicSignUp(session);
+        mockMvc.perform(
+                        post("/api/resource/move")
+                                .param("from", "exam/test.txt")
+                                .param("to", "test/test.txt")
+                                .session(session))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", containsString("is not found")));
+    }
+
+    @Test
+    @Disabled
+    void testMoveFile_conflict() throws Exception {
+        basicSignUp(session);
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "test.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "hello".getBytes()
+        );
+
+        mockMvc.perform(
+                        multipart("/api/resource")
+                                .file(file)
+                                .param("path", "test")
+                                .session(session))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(
+                        multipart("/api/resource")
+                                .file(file)
+                                .param("path", "exam")
+                                .session(session))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(
+                        post("/api/resource/move")
+                                .param("from", "test/test.txt")
+                                .param("to", "exam/test.txt")
+                                .session(session))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.message", containsString("already exists")));
+    }
+
+    @Test
+    void testMoveFile_move_success() throws Exception {
+        basicSignUp(session);
+
+        byte[] content = "hello".getBytes();
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "test.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                content
+        );
+
+        mockMvc.perform(
+                        multipart("/api/resource")
+                                .file(file)
+                                .param("path", "test")
+                                .session(session))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(
+                        post("/api/resource/move")
+                                .param("from", "test/test.txt")
+                                .param("to", "exam/test.txt")
+                                .session(session))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size").value(content.length))
+                .andExpect(jsonPath("$.path").value("exam/"))
+                .andExpect(jsonPath("$.name").value("test.txt"))
+                .andExpect(jsonPath("$.type").value("FILE"));
+
+        mockMvc.perform(
+                        get("/api/resource")
+                                .param("path", "test/test.txt")
+                                .session(session))
+                .andExpect(status().isNotFound());
+
+        mockMvc.perform(
+                        get("/api/resource")
+                                .param("path", "exam/test.txt")
+                                .session(session))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("test.txt"));
+    }
+
+    @Test
+    void testMoveFile_rename_success() throws Exception {
+        basicSignUp(session);
+
+        byte[] content = "hello".getBytes();
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file",
+                "test.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                content
+        );
+
+        mockMvc.perform(
+                        multipart("/api/resource")
+                                .file(file)
+                                .param("path", "test")
+                                .session(session))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(
+                        post("/api/resource/move")
+                                .param("from", "test/test.txt")
+                                .param("to", "test/new_test.txt")
+                                .session(session))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size").value(content.length))
+                .andExpect(jsonPath("$.path").value("test/"))
+                .andExpect(jsonPath("$.name").value("new_test.txt"))
+                .andExpect(jsonPath("$.type").value("FILE"));
+
+        mockMvc.perform(
+                        get("/api/resource")
+                                .param("path", "test/test.txt")
+                                .session(session))
+                .andExpect(status().isNotFound());
+
+        mockMvc.perform(
+                        get("/api/resource")
+                                .param("path", "test/new_test.txt")
+                                .session(session))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("new_test.txt"));
     }
 }
