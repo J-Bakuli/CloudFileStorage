2860c998d13f50d4eb5fa21343c850a65a3fc608
J-Bakuli <julia.bakuli@yandex.ru>
2026-08-02 22:25:40 +0300

    Move RounterFunctionService to config part

--- src/main/java/com/jb/cloudstorage/cloud_storage/config/RouterFunctionConfig.java
+++ src/main/java/com/jb/cloudstorage/cloud_storage/config/RouterFunctionConfig.java
@@ -1,15 +1,15 @@
-package com.jb.cloudstorage.cloud_storage.service;
+package com.jb.cloudstorage.cloud_storage.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.function.RouterFunction;
 
 import static org.springframework.web.servlet.function.RequestPredicates.path;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
-@Service
-public class RouterFunctionService {
+@Configuration
+public class RouterFunctionConfig {
     @Bean
     RouterFunction spaRouter() {
         ClassPathResource index = new ClassPathResource("static/index.html");
