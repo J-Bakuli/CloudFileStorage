8f2917c88701aec2a4883893350193f60009705e
J-Bakuli <julia.bakuli@yandex.ru>
2026-08-13 10:46:26 +0300

    Change Exception with Throwable in StorageException constructor

--- src/main/java/com/jb/cloudstorage/cloud_storage/exception/StorageException.java
+++ src/main/java/com/jb/cloudstorage/cloud_storage/exception/StorageException.java
@@ -1,7 +1,7 @@
 package com.jb.cloudstorage.cloud_storage.exception;
 
 public class StorageException extends RuntimeException {
-    public StorageException(String message, Exception e) {
-        super(message, e);
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
