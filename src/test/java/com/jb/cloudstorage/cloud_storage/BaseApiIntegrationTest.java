66b22fcbc29cbe9a89f5313431b497707df805f7
J-Bakuli <julia.bakuli@yandex.ru>
2026-08-19 13:51:56 +0300

    Remove empty @TestContainers

--- src/test/java/com/jb/cloudstorage/cloud_storage/BaseApiIntegrationTest.java
+++ src/test/java/com/jb/cloudstorage/cloud_storage/BaseApiIntegrationTest.java
@@ -27,7 +27,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-@Testcontainers
 @SpringBootTest
 @EnableAutoConfiguration(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 @Transactional
