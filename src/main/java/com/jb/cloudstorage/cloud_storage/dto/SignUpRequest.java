fce4250f56063c9efcd0b16baa220dabf93b2bf6
J-Bakuli <julia.bakuli@yandex.ru>
2026-08-19 11:30:08 +0300

    Change SignInRequest, SignUpRequest message wording

--- src/main/java/com/jb/cloudstorage/cloud_storage/dto/SignUpRequest.java
+++ src/main/java/com/jb/cloudstorage/cloud_storage/dto/SignUpRequest.java
@@ -6,14 +6,14 @@ import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
         @NotBlank
-        @Size(min = 5, max = 20, message = "Username is to contain from 5 to 20 characters")
+        @Size(min = 5, max = 20, message = "Username must contain from 5 to 20 characters")
         @Pattern(
                 regexp = "^[a-zA-Z0-9]+[a-zA-Z_0-9]*[a-zA-Z0-9]+$",
                 message = "Username contains invalid characters"
         )
         String username,
         @NotBlank
-        @Size(min = 5, max = 20, message = "Password is to contain from 5 to 20 characters")
+        @Size(min = 5, max = 20, message = "Password must contain from 5 to 20 characters")
         @Pattern(
                 regexp = "^[a-zA-Z0-9!@#$%^&*(),.?\":{}|<>\\[\\]\\/`~+=\\-_';]*$",
                 message = "Password contains invalid characters"
